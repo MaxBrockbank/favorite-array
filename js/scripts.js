262d3ebde084f483c1adc5ebdd36252ae4aaacd9
Eric Stratton <strattonericj@gmail.com>
2020-10-26 11:56:58 -0700

    use bracket notation to move 3 items to new array and print them to a list on html
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,20 @@ $(document).ready(function () {
     const favorite3Input = $("input#favorite3").val();
     const favorite4Input = $("input#favorite4").val();
 
-    // $(".favorite1").text(favorite1Input);
-    // $(".favorite2").text(favorite2Input);
-    // $(".favorite3").text(favorite3Input);
-    // $(".favorite4").text(favorite4Input);
-
     let favoriteArray = [favorite1Input, favorite2Input, favorite3Input, favorite4Input];
     console.log(favoriteArray);
 
 
-    let secondFirstThird = favoriteArray.slice(0, 3);
+    let firstSecondThird = favoriteArray.slice(0, 3);
+
+    const secondArray = [];
+    secondArray.push(favoriteArray[1]);
+    secondArray.push(favoriteArray[0]);
+    secondArray.push(favoriteArray[2]);
+
+    $("li#1").text(secondArray[0]);
+    $("li#2").text(secondArray[1]);
+    $("li#3").text(secondArray[2]);
 
   });
 });
