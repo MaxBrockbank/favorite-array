7649d0c0ce0a5ff7783aaeb2c5364c39b058102d
Eric Stratton <strattonericj@gmail.com>
2020-10-26 11:00:03 -0700

    add initial html, css, and js, inplement js to take user input and place into an array
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("#userFavorites").submit(function (event) {
+    event.preventDefault();
+    const favorite1Input = $("input#favorite1").val();
+    const favorite2Input = $("input#favorite2").val();
+    const favorite3Input = $("input#favorite3").val();
+    const favorite4Input = $("input#favorite4").val();
+
+    // $(".favorite1").text(favorite1Input);
+    // $(".favorite2").text(favorite2Input);
+    // $(".favorite3").text(favorite3Input);
+    // $(".favorite4").text(favorite4Input);
+
+
+
+    let favoriteArray = [favorite1Input, favorite2Input, favorite3Input, favorite4Input];
+
+
+  });
+});
